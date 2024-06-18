9a6b35b09907ee98ec0a7f110a8f62e30099e1b5
blyqq <f0ul2212@mail.ru>
2024-06-19 02:29:35 +0300

    update docstring

--- note_manager/crud.py
+++ note_manager/crud.py
@@ -4,14 +4,44 @@ from . import models, schemas
 
 
 def get_note_by_id(db: Session, note_id: int):
+    """Получает заметку по её идентификатору.
+
+    Args:
+        db (Session): Сессия базы данных SQLAlchemy.
+        note_id (int): ID заметки.
+
+    Returns:
+        models.Note: Заметка с указанным идентификатором, если найдена, иначе None.
+    """
     return db.query(models.Note).filter(models.Note.id == note_id).first()
 
 
 def get_notes(db: Session, skip: int = 0, limit: int = 10):
+    """
+    Возвращает список всех заметок с возможностью пагинации.
+
+    Args:
+        db (Session): Сессия базы данных SQLAlchemy.
+        skip (int, optional): Количество заметок для пропуска. Defaults to 0.
+        limit (int, optional): Максимальное количество заметок для возврата. Defaults to 10.
+
+    Returns:
+        list[models.Note]: Список заметок.
+    """
     return db.query(models.Note).offset(skip).limit(limit).all()
 
 
 def create_note(db: Session, note: schemas.NoteCreate):
+    """
+    Создает новую заметку в базе данных.
+
+    Args:
+        db (Session): Сессия базы данных SQLAlchemy.
+        note (schemas.NoteCreate): Данные новой заметки.
+
+    Returns:
+        models.Note: Созданная заметка.
+    """
     db_note = models.Note(title=note.title, content=note.content)
     db.add(db_note)
     db.commit()
@@ -20,6 +50,16 @@ def create_note(db: Session, note: schemas.NoteCreate):
 
 
 def delete_note_by_id(db: Session, note_id: int):
+    """
+    Удаляет заметку из базы данных по её ID.
+
+    Args:
+        db (Session): Сессия базы данных SQLAlchemy.
+        note_id (int): ID заметки.
+
+    Returns:
+        bool: True, если заметка успешно удалена, иначе False.
+    """
     db_note = db.query(models.Note).filter(models.Note.id == note_id).first()
     if db_note:
         db.delete(db_note)
@@ -28,6 +68,16 @@ def delete_note_by_id(db: Session, note_id: int):
     return False
 
 def search_notes(db: Session, keyword: str):
+    """
+    Ищет заметки по ключевому слову или фразе в заголовке или содержании.
+
+    Args:
+        db (Session): Сессия базы данных SQLAlchemy.
+        keyword (str): Ключевое слово или фраза для поиска.
+
+    Returns:
+        list[models.Note]: Список заметок, содержащих указанное ключевое слово или фразу.
+    """
     return db.query(models.Note).filter(
         or_(
             models.Note.title.contains(keyword),
--- note_manager/main.py
+++ note_manager/main.py
@@ -11,6 +11,11 @@ app = FastAPI()
 
 
 def get_db():
+    """ Функция зависимости для получения экземпляра сессии базы данных
+
+    Yields:
+         Session: Сессия базы данных SQLAlchemy.
+    """
     db = SessionLocal()
     try:
         yield db
@@ -20,16 +25,47 @@ def get_db():
         
 @app.post("/notes/", response_model=schemas.Note)
 def create_note(note: schemas.NoteCreate, db: Session = Depends(get_db)):
+    """ Создает новую заметку в базе данных.
+
+    Args:
+        note (schemas.NoteCreate): Данные новой заметки.
+        db (Session, optional): Сессия базы данных SQLAlchemy. Defaults to Depends(get_db).
+
+    Returns:
+        schemas.Note: Созданная заметка.
+    """
     return crud.create_note(db=db, note=note)
 
 
 @app.get("/notes/", response_model=list[schemas.Note])
 def read_notes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    """ Возвращает список всех заметок.
+
+    Args:
+        skip (int, optional): Количество заметок для пропуска. Defaults to 0.
+        limit (int, optional): Максимальное количество заметок для возврата. Defaults to 10.
+        db (Session, optional): Сессия базы данных SQLAlchemy. Defaults to Depends(get_db).
+
+    Returns:
+         list[schemas.Note]: Список заметок.
+    """
     notes = crud.get_notes(db, skip=skip, limit=limit)
     return notes
     
 @app.get("/notes/{note_id}", response_model=schemas.Note)
 def read_note(note_id: int, db: Session = Depends(get_db)):
+    """Возвращает заметку по её ID.
+
+    Args:
+        note_id (int): ID заметки.
+        db (Session, optional): Сессия базы данных SQLAlchemy. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: Записка не найдена
+
+    Returns:
+        schemas.Note: Заметка с указанным ID.
+    """
     db_note = crud.get_note_by_id(db, note_id=note_id)
     if db_note is None:
         raise HTTPException(status_code=404, detail="Note not found")
@@ -38,6 +74,19 @@ def read_note(note_id: int, db: Session = Depends(get_db)):
 
 @app.delete("/notes/{note_id}", response_model=schemas.Note)
 def delete_note(note_id: int, db: Session = Depends(get_db)):
+    """ Удаляет заметку по её ID.
+
+    Args:
+        note_id (int): ID заметки.
+        db (Session, optional): Сессия базы данных SQLAlchemy. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: Заметка не найдена
+        HTTPException: Не удалось удалить заметку
+
+    Returns:
+        schemas.Note: Удалённая заметка.
+    """
     db_note = crud.get_note_by_id(db, note_id=note_id)
     if db_note is None:
         raise HTTPException(status_code=404, detail="Note not found")
@@ -49,5 +98,15 @@ def delete_note(note_id: int, db: Session = Depends(get_db)):
 
 @app.get("/notes/search/", response_model=list[schemas.Note])
 def search_notes(keyword: str = Query(..., min_length=1), db: Session = Depends(get_db)):
+    """Поиск заметок по ключевому слову или фразе в заголовке или содержании.
+
+
+    Args:
+        keyword (str, optional): Ключевое слово или фраза для поиска.
+        db (Session, optional): Сессия базы данных SQLAlchemy. Defaults to Depends(get_db).
+
+    Returns:
+        list[schemas.Note]: Список заметок, содержащих указанное ключевое слово или фразу.
+    """
     notes = crud.search_notes(db=db, keyword=keyword)
     return notes
